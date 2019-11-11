c74f505f3dfb03b5a278344678a74f549ce34796
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-11 01:32:27 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,1 @@
-UPDATE characters.species SET species = 'Martian' WHERE characters.species = 'cylon';
+UPDATE characters.species SET characters.species = 'Martian' WHERE characters.species = 'cylon';
