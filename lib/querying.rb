8b3f8506ac6c50282f71c642fc38171d0edcc560
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-11 03:51:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM (SELECT series.title AS title,  );"
+  "SELECT title, species_count FROM (SELECT series.title AS title, COUNT(characters.species) AS species_count FROM species INNER JOIN characters GROUP BY characters.species);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
