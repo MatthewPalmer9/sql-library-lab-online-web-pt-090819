0530d03d71274b0b4d06c2b5a4f895d67df3da71
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-11 01:06:31 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY
+);
